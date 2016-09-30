dc6a545cce2770bf728eeb01a8c9de238e6cb4e2
Edward Chu <crazyzyt@gmail.com>
2016-09-30 11:55:29 +0800

    enhanced performances and experiences

--- themes/fractal/source/js/mods/common.js
+++ themes/fractal/source/js/mods/common.js
@@ -40,16 +40,54 @@
     App.$win.on('resize', function() {
         onResize();
         App.$win.trigger('siteResized');
-    }).on('load', function() {
-        onResize();
-        loader.destroy();
-        App.$win.trigger('siteLoaded');
     }).on('loaderGone', function() {
         App.$htmlBody.removeClass('ovf-hidden');
         App.$body.addClass('loaded');
         goToHash();
     });
 
+    function finishLoaded () {
+        console.log('Site finish loaded.') 
+        onResize();
+        loader.destroy();
+        App.$win.trigger('siteLoaded');
+    }
+
+    function waitForLoaded () {
+        var imgElems = document.querySelectorAll('.wait-for-img img')
+        if (!imgElems.length) {
+            console.log('Finish loaded without any images waiting.')
+            return finishLoaded()
+        }
+
+        var doneCount = 0, timeoutToken
+        function imgOnLoadHandler () {
+            doneCount++
+            if (doneCount === imgElems.length) {
+                timeoutToken && clearTimeout(timeoutToken)
+                finishLoaded()
+            }
+        }
+
+        // still reveal page if images are loaded overtime
+        timeoutToken = setTimeout(function () {
+            console.log('Images loaded overtime, finished: %s/%s', doneCount, imgElems.length)
+            finishLoaded()
+        }, 6000)
+        
+        // track down images fetch status
+        Array.prototype.slice.call(imgElems).forEach(function (img) {
+            if (img.complete) {
+                return imgOnLoadHandler()
+            } else {
+                img.onload = imgOnLoadHandler
+                img.onerror = imgOnLoadHandler
+                img.onabort = imgOnLoadHandler
+            }
+        })
+    }
+
+    waitForLoaded();
     onResize();
 
 })(jQuery, App);
