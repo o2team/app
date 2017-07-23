7ea92c9b0afe2fb0c2e7269b3d36727561807a02
JChehe <574805242@qq.com>
2017-07-23 12:02:00 +0800

    gg

--- themes/fractal/source/js/index.js
+++ themes/fractal/source/js/index.js
@@ -1,14 +1,35 @@
-(function($, App) {
-    $('.scrolly').on('click', function() {
-        scrollTo(this.getAttribute('href'));
-        return false;
+(function($) {
+  $(function () {
+    var $intro = $('.intro')
+    var $introOpts = $('.intro--options a')
+    var $introBannerTag = $('.intro--banner-tag')
+    var introDuration = 5000
+    var introOptsActiveIndex = 0
+    // 初始化状态
+    $introOpts.eq(introOptsActiveIndex).addClass('is-active')
+    $intro.attr('data-active-index', introOptsActiveIndex)
+    $("#loader").fadeOut('slow', function() {
+        $(this).remove();
+        // App.$win.trigger('loaderne');
     });
 
+    setInterval(function () {
+      $introOpts.removeClass('is-active')
+      introOptsActiveIndex++
+      if (introOptsActiveIndex > $introOpts.length - 1) {
+        introOptsActiveIndex = 0
+      }
+      $intro.attr('data-active-index', introOptsActiveIndex)
+      var left = $introOpts.eq(introOptsActiveIndex).position().left
+      $introBannerTag.css({
+        '-webkit-transform': 'translateX(' + left + 'px)',
+        '-moz-transform': 'translateX(' + left + 'px)',
+        '-ms-transform': 'translateX(' + left + 'px)',
+        '-o-transform': 'translateX(' + left + 'px)',
+        'transform': 'translateX(' + left + 'px)'
+      })
+    }, introDuration)
 
-    var scrollTo = function(id) {
-        App.$htmlBody.animate({
-            scrollTop: $(id).offset().top
-        }, 1000);
-    };
+})
 
-})(jQuery, App);
+})(jQuery);
